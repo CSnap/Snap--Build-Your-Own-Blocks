d7c4aef2aa46be6390bd8939ea6d75d3a9982394
Andrew Hunn <ahunn@umich.edu>
2019-10-30 13:10:03 -0400

    Add Sports and Animals, resize images

--- gui.js
+++ gui.js
@@ -106,7 +106,15 @@ var Costumes_2D = {
 	"Four Directions": {
         "directory": "fourdirections",
         "src": ["head.png","human.png", "kite.png", "medicinewheel.png", "rhombus1.png", "rhombus2.png", "triangle1.png", "triangle2.png", "turtle_island.png", "turtlefoot.png", "turtlehead.png"]
+    },	
+    "Sports": {
+        "directory": "sports",
+        "src": ["motion0.png", "motion1.png", "motion2.png", "motion3.png", "motion4.png"]
     },
+    "Animals": {
+        "directory": "animals",
+        "src": ["bear.png", "buffalo.png", "deer.png", "eagle.png", "loon.png"]
+    }
 };
 
 // IDE_Morph ///////////////////////////////////////////////////////////
